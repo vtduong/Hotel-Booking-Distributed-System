65e66ee75c91a7cd952d6249f8e9008b18ec739e
code-asc <avgchowdary@gmail.com>
2019-07-15 19:08:30 -0400

    created configuration tool for udp address over LAN.

---
+++ src/frontend/FrontEnd.java
@@ -0,0 +1,48 @@
+package frontend;
+
+public class FrontEnd {
+	
+	
+	  public String addEvent (String eventID, String eventType, int bookingCapacity) {
+		  //TODO
+		  return null;
+	  }
+	  
+	  
+	  public String removeEvent (String eventID, String eventType) {
+		//TODO
+		  return null;
+	  }
+	  
+	  
+	  public String listEventAvailability (String eventType) {
+		//TODO
+		  return null;
+	  }
+	  
+	  
+	  public String bookEvent (String customerID, String eventID, String eventType) {
+		//TODO
+		  return null;
+	  }
+	  
+	  
+	  public String getBookingSchedule (String customerID) {
+		//TODO
+		  return null;
+	  }
+	  
+	  
+	  public String cancelEvent (String customerID, String eventID, String eventType) {
+		//TODO
+		  return null;
+	  }
+	  
+	  
+	  public String swapEvent (String customerID, String newEventID, String newEventType, String oldEventID, String oldEventType) {
+		//TODO
+		  return null;
+	  }
+
+	
+}
