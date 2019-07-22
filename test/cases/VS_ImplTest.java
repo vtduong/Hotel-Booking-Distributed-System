0f1debb732cf71a476f01a8e9162da014fa95c93
Van Duong <vanduong1610@gmail.com>
2019-07-22 18:15:49 -0400

    modify test

--- test/cases/VS_ImplTest.java
+++ test/cases/VS_ImplTest.java
@@ -5,12 +5,16 @@ package cases;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.ORBPackage.InvalidName;
@@ -19,9 +23,9 @@ import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
 
-import aspackage.clientServer.MTL;
-import aspackage.clientServer.OTW;
-import aspackage.clientServer.TOR;
+import vspackage.server.MTL;
+import vspackage.server.OTW;
+import vspackage.server.TOR;
 import vspackage.RemoteMethodApp.RemoteMethodHelper;
 import vspackage.RemoteMethodApp.RemoteMethodPackage.AccessDeniedException;
 import vspackage.RemoteMethodApp.RemoteMethodPackage.ClassNotFoundException;
@@ -37,15 +41,15 @@ import vspackage.server.MethodImpl;
  */
 public class VS_ImplTest {
 
-	private vspackage.RemoteMethodApp.RemoteMethod otw = null;
-	private vspackage.RemoteMethodApp.RemoteMethod mtl = null;
-	private vspackage.RemoteMethodApp.RemoteMethod tor = null;
+//	private vspackage.RemoteMethodApp.RemoteMethod otw = null;
+//	private vspackage.RemoteMethodApp.RemoteMethod mtl = null;
+//	private vspackage.RemoteMethodApp.RemoteMethod tor = null;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		
+		//TODO start servers before run tests
 	}
 
 	/**
@@ -53,16 +57,28 @@ public class VS_ImplTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
-
+		clearClassData("MTL");
+		clearClassData("TOR");
+		clearClassData("OTW");
 	}
 
+	@Ignore
 	private void clearClassData(String name) throws NoSuchFieldException, SecurityException, java.lang.ClassNotFoundException, IllegalArgumentException, IllegalAccessException {
 		// Get the private String field
 		final Field field = Class.forName(name).getDeclaredField("eventMap");
 		// Allow modification on the field
 		field.setAccessible(true);
 		// Get
-		final Object oldValue = field.get(Class.forName(MTL.class.getName()));
+		final Object oldValue = field.get(Class.forName(name).getName());
+		// Sets the field to the new value
+		field.set(oldValue, null);
+		
+		// Get the private String field
+		final Field field1 = Class.forName(name).getDeclaredField("eventCus");
+		// Allow modification on the field
+		field.setAccessible(true);
+		// Get
+		final Object oldValue2 = field.get(Class.forName(name).getName());
 		// Sets the field to the new value
 		field.set(oldValue, null);
 		
@@ -81,16 +97,23 @@ public class VS_ImplTest {
 		//		Method method = Client.class.getDeclaredMethod("connectServer", String.class, Integer.class, String.class);
 //		method.setAccessible(true);
 //		method.invoke(client, hos, Protocol.ADD_EVENT, "OTWM4560");		
-		System.out.println("Lookup completed " );
-		
-		String result = otw.addEvent("OTWE080619", "Conference", 1);
-		assertTrue(result.contains("successfully"));
-		result = otw.addEvent("OTWE110619", "Conference", 1);
-		assertTrue(result.contains("successfully"));
-		result = otw.addEvent("TORE050619", "Conference", 1);
-		assertFalse(result.contains("successfully"));
+//		String result = otw.addEvent("OTWE080619", "Conference", 1);
+//		assertTrue(result.contains("successfully"));
+//		result = otw.addEvent("OTWE110619", "Conference", 1);
+//		assertTrue(result.contains("successfully"));
+//		result = otw.addEvent("TORE050619", "Conference", 1);
+//		assertFalse(result.contains("successfully"));
 		
 		
+	}
+	
+	@Test
+	public void Test() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Constructor<MethodImpl> constructor;
+        constructor = MethodImpl.class.getDeclaredConstructor(Object.class);
+        constructor.setAccessible(true);
+        MethodImpl mtl = constructor.newInstance("MTL");
+        
 	}
 	
 	
